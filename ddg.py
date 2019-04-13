6385da2328cf241f113f19c8320bfe6d01b682f5
Kaiser <34060097+Kaiz0r@users.noreply.github.com>
2019-04-13 11:53:58 +0100

    Add files via upload

---
+++ ddg.py
@@ -0,0 +1,46 @@
+from . import commands 
+import random
+
+class DuckDuckGo(commands.BaseCommand):
+	def __init__(self, host):
+		super().__init__(host)
+		
+		try:
+			import duckduckgo
+			self.client = duckduckgo.DDG('AOSR')
+			
+		except Exception as e:
+			print(e)
+			print("DDG API not available. Download from http://github.com/codedthoughts/duckduckgo")
+			self.disabled = True
+			
+		self.addListener("ddg")
+		self.addListener("search for ")
+		self.inter = True
+	
+	def doSearch(self, value):
+			num = -1
+			for i in range(0, 10):
+				if f'/{i}' in value:
+					value = value.replace(f'/{i}', '')
+					num = i
+			
+			if num == -1:
+				num = random.randint(0, 10)
+
+			data = self.client.search(value)
+			
+			#print(data['snippets'][num])
+			#print(data['urls'][num])
+			return f"{self.host.formatting.Bold}[Search for {value} (RESULT: {num})]{self.host.reset_f}\n{data['snippets'][num]}\nhttp://{data['urls'][num]}"
+	
+	def onHeld(self, value):
+		return self.output(self.doSearch(value))
+	
+	def onTrigger(self, value = ""):
+		if self.disabled:
+			return self.message("DDG API not available. Download from http://github.com/codedthoughts/duckduckgo")
+		
+		if value == "":
+			return self.hold("Search for what?")
+		return self.output(self.doSearch(value))
